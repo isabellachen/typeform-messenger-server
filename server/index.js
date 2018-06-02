46623f4d232c51bdff4a4c2eac6998756d11b0ba
Isabella Chen <miss.izzyc@gmail.com>
2018-06-02 22:03:36 +0200

    chore: updated gitignore, package.json, logo

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
 
 const router = require('./router') 
 
 const app = new Koa()
 
-app.use(router())
+app
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3000, () => console.log('koa app listening on port 3000'))
