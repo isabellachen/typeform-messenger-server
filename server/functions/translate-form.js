7ab90ac4eae50ad1d6f3c04b04d8407bffa6bd01
Isabella Chen <miss.izzyc@gmail.com>
2018-09-07 13:15:36 +0200

    feat: add handling for thank you screen

--- server/functions/translate-form.js
+++ server/functions/translate-form.js
@@ -1,58 +1,84 @@
-const translateFunctions = require('./translate-functions')
-const { translateWelcomeScreen,
-        translateShortText,
-        translateMultipleChoice,
-      } = translateFunctions
+const {
+  translateWelcomeScreen,
+  translateThankYouScreen,
+  translateShortText,
+  translateMultipleChoice,
+  translateEmail,
+  translatePictureChoice,
+  translateLongText
+} = require('typeform-to-facebook-messenger');
 
-const translateForm = (data) => {
+const translateForm = data => {
+  const welcomeScreen = [];
+  const thankYouScreen = [];
 
-  const welcomeScreen = []
   if (data.welcome_screens) {
-    const welcome = data.welcome_screens[0]
+    const welcome = data.welcome_screens[0];
     let response = {
       response: translateWelcomeScreen(welcome),
       type: 'welcome_screen',
-      ref: welcome.ref,
-    }
-    welcomeScreen.push(response)
+      ref: welcome.ref
+    };
+    welcomeScreen.push(response);
+  }
+
+  if (data.thankyou_screens) {
+    const thankYou = data.thankyou_screens[0];
+    let response = {
+      response: translateThankYouScreen(thankYou),
+      type: 'thankyou_screen',
+      ref: thankYou.ref
+    };
+    thankYouScreen.push(response);
   }
 
   const translatedQuestions = data.fields.map(question => {
-    const response = {} 
+    const response = {};
     if (question.type === 'short_text') {
       return {
         response: translateShortText(question),
         type: question.type,
-        ref: question.ref,
-      }
+        ref: question.ref
+      };
     }
     if (question.type === 'multiple_choice') {
       return {
         response: translateMultipleChoice(question),
         type: question.type,
-        ref: question.ref,
-      }
+        ref: question.ref
+      };
     }
     if (question.type === 'email') {
-      response.text = question.title
-      return response //to be deleted
+      question.title = question.title.split(',')[1]; //TODO: quick fix for email field
+      return {
+        response: translateEmail(question),
+        type: question.type,
+        ref: question.ref
+      };
     }
     if (question.type === 'picture_choice') {
-      response.text = question.title
-      return response //to be deleted
+      return {
+        response: translatePictureChoice(question),
+        type: question.type,
+        ref: question.ref
+      };
     }
     if (question.type === 'long_text') {
-      response.text = question.title
-      return response //to be deleted
+      return {
+        response: translateLongText(question),
+        type: question.type,
+        ref: question.ref
+      };
     }
-  })
+  });
+
   const translatedForm = [
-    ...welcomeScreen, 
-    ...translatedQuestions
-  ]
+    ...welcomeScreen,
+    ...translatedQuestions,
+    ...thankYouScreen
+  ];
 
-  return translatedForm
-}
+  return translatedForm;
+};
 
-
-module.exports = translateForm
+module.exports = translateForm;
